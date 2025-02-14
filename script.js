727fa78b88265c743f2b76c3759208df0e9e07f2
zartul <121471819+zartul@users.noreply.github.com>
2025-02-14 09:19:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+    let val = document.getElementById("sampleTable");
+    let newRow = val.insertRow(0);
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
   
 }
